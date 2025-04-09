3406cedcbf89104ff4b4c0d1b2c5590f40203bc6
Magnus <678288@stud.hvl.no>
2025-04-09 19:59:31 +0200

    fix

--- src/main/java/com/br/betterreads/controller/BookController.java
+++ src/main/java/com/br/betterreads/controller/BookController.java
@@ -17,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
